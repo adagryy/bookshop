98be5927ac7c595396a66c1d6c7092e0f474eba3
Estera Kot <esterakot@gmail.com>
2015-11-30 00:32:17 +0100

    Failed (yet) test.

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,3 +1,18 @@
+import models.BooksEntity;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.junit.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.math.BigDecimal;
+
 /**
  * Created by ekot on 29.11.15.
  */
@@ -10,6 +25,15 @@ public class MainTest {
 
     @org.junit.Test
     public void testAddRecord() throws Exception {
+
+        Transaction trns = null;
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        trns = session.beginTransaction();
+
+        Main m = new Main();
+        BooksEntity be = new BooksEntity();
+
+        assertTrue(session.save(be) instanceof BooksEntity);
 
     }
 
