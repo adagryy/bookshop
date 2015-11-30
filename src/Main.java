ea4845fb0db17dc02af4beee52491ccad0162db1
Estera Kot <esterakot@gmail.com>
2015-11-30 21:08:40 +0100

    Get + returny do metod + Testy prawie działające

--- src/Main.java
+++ src/Main.java
@@ -19,10 +19,9 @@ import java.math.BigDecimal;
 public class Main {
     public static void main(final String[] args) throws Exception {
         Main m = new Main();
-
         BooksEntity be = new BooksEntity();
 
-        int record = 10;//numer rekordu w tabeli do wpisywania lub updateowania
+        int record = 15;//numer rekordu w tabeli do wpisywania lub updateowania
 
         be.setId(record);
         be.setTitle("The lord of the Rings");
@@ -30,72 +29,70 @@ public class Main {
         be.setPrice(new BigDecimal(9.99));
         be.setKategoria(2);
         be.setWydawnictwo_id(3);
-
-        m.addRecord(be);
-
+        m.get(8);
+        //m.addRecord(be);
         //m.updateRecord(be);
 
         //m.deleteRecord(record);
-
-        System.out.println(m.getRecordById(5));
-
-        for (BooksEntity iter : m.getAllRecords()) {
-            System.out.println(iter);
-        }
+//        System.out.println(m.getRecordById(5));
+//        for (BooksEntity iter : m.getAllRecords()) {
+//            System.out.println(iter);
+//        }
     }
 
-    public List<BooksEntity> getAllRecords(){
-        List<BooksEntity> books = new ArrayList<BooksEntity>();
-        Transaction trns = null;
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        try {
-            trns = session.beginTransaction();
-            books = session.createQuery("from books").list();
-        } catch (RuntimeException e) {
-            e.printStackTrace();
-        } finally {
-            session.flush();
-            session.close();
-        }
-        return books;
-    }
+//    public List<BooksEntity> getAllRecords(){
+//        List<BooksEntity> books = new ArrayList<BooksEntity>();
+//        Transaction trns = null;
+//        Session session = HibernateUtil.getSessionFactory().openSession();
+//        try {
+//            trns = session.beginTransaction();
+//            books = session.createQuery("from books").list();
+//        } catch (RuntimeException e) {
+//            e.printStackTrace();
+//        } finally {
+//            session.flush();
+//            session.close();
+//        }
+//        return books;
+//    }
 
-    public BooksEntity getRecordById(int record_id){
-        BooksEntity book = null;
-        Transaction trns = null;
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        try {
-            trns = session.beginTransaction();
-            String queryString = "from books where id = :id";
-            Query query = session.createQuery(queryString);
-            query.setInteger("id", record_id);
-            book = (BooksEntity) query.uniqueResult();
-        } catch (RuntimeException e) {
-            e.printStackTrace();
-        } finally {
-            session.flush();
-            session.close();
-        }
-        return book;
-    }
+//    public BooksEntity getRecordById(int record_id){
+//        BooksEntity book = null;
+//        Transaction trns = null;
+//        Session session = HibernateUtil.getSessionFactory().openSession();
+//        try {
+//            trns = session.beginTransaction();
+//            String queryString = "from books where id = :id";
+//            Query query = session.createQuery(queryString);
+//            query.setInteger("id", record_id);
+//            book = (BooksEntity) query.uniqueResult();
+//        } catch (RuntimeException e) {
+//            e.printStackTrace();
+//        } finally {
+//            session.flush();
+//            session.close();
+//        }
+//        return book;
+//    }
 
-    public void addRecord(BooksEntity be) {
+    public Integer addRecord(BooksEntity be) {
         Transaction trns = null;
         Session session = HibernateUtil.getSessionFactory().openSession();
+        Integer id = null;
         try {
             trns = session.beginTransaction();
-            session.save(be);
+            id = (Integer) session.save(be);
             session.getTransaction().commit();
         } catch (RuntimeException e) {
             if (trns != null) {
                 trns.rollback();
             }
             e.printStackTrace();
-            //System.out.println("Error");
         } finally {
             session.flush();
             session.close();
         }
+        return id;
     }
 
     public void deleteRecord(int record_id) {
@@ -133,5 +130,27 @@ public class Main {
             session.flush();
             session.close();
         }
+    }
+    public BooksEntity get(Integer id) {
+        Transaction trns = null;
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        BooksEntity returned = null;
+        try {
+            trns = session.beginTransaction();
+            returned = (BooksEntity) session.get(BooksEntity.class, id);
+            session.getTransaction().commit();
+        } catch (RuntimeException e) {
+            if (trns != null) {
+                trns.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.flush();
+            session.close();
+        }
+        System.out.println(returned.getAuthor());
+        System.out.println(returned.getKategoria());
+        System.out.println(returned.getTitle());
+        return returned;
     }
 }
--- src/MainTest.java
+++ src/MainTest.java
@@ -1,22 +1,27 @@
 import models.BooksEntity;
+import models.BooksEntity;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.junit.*;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
 import java.util.Iterator;
 import java.util.Set;
 import java.math.BigDecimal;
+import java.math.BigDecimal;
+import org.junit.Test;
+import org.junit.*;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by ekot on 29.11.15.
  */
 public class MainTest {
+
+    private Main m = new Main();
 
     @org.junit.Test
     public void testMain() throws Exception {
@@ -25,17 +30,32 @@ public class MainTest {
 
     @org.junit.Test
     public void testAddRecord() throws Exception {
+        BooksEntity book = createBook();
+        Integer id = m.addRecord(book);
 
-        Transaction trns = null;
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        trns = session.beginTransaction();
-
-        Main m = new Main();
-        BooksEntity be = new BooksEntity();
-
-        assertTrue(session.save(be) instanceof BooksEntity);
-
+        // get
+        BooksEntity bookFromDb = m.get(id);
+        validate(book, bookFromDb);
     }
+
+
+    protected BooksEntity createBook(){
+        BooksEntity be = new BooksEntity();
+        be.setId(15);
+        be.setTitle("The lord of the Rings");
+        be.setAuthor("Jakis tam");
+        be.setPrice(new BigDecimal(9.99));
+        be.setKategoria(2);
+        be.setWydawnictwo_id(3);
+        return be;
+    }
+
+    private void validate(BooksEntity expected, BooksEntity actual){
+        assertEquals(expected.getAuthor(), actual.getAuthor());
+        assertEquals(expected.getKategoria(), actual.getKategoria());
+    }
+
+
 
     @org.junit.Test
     public void testDeleteRecord() throws Exception {
